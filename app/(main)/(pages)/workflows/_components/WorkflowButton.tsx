cccac02d09b2ddf2663c4b256107dafe50c5ee7b
harshit Mishra <miananya2@gmail.com>
2024-11-29 01:20:30 +0530

    connections page

---
+++ app/(main)/(pages)/workflows/_components/WorkflowButton.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+
+export const WorkflowButton = () => {
+  return (
+    <Button size={"icon"}>
+      <Plus />
+    </Button>
+  );
+};
---
+++ types/index.ts
@@ -0,0 +1,13 @@
+import { ConnectionProviderProps } from "@/providers/ConnectionProvider";
+
+export type ConnectionTypes = "Google Drive" | "Notion" | "Slack" | "Discord";
+
+export type Connection = {
+  title: ConnectionTypes;
+  description: string;
+  image: string;
+  connectionKey: keyof ConnectionProviderProps;
+  accessTokenKey?: string;
+  alwaysTrue?: boolean;
+  slackSpecial?: boolean;
+};
